d99991842b8af3d220da252fcb367d355a173cba
IT教程资源 <3092994143@qq.com>
2021-01-31 15:08:52 +0800

    提交

--- shiropro/src/main/java/shiroDemo.java
+++ shiropro/src/main/java/shiroDemo.java
@@ -1,8 +1,10 @@
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
+import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 
 /**
  * @author liyaoping
@@ -15,6 +17,8 @@ public class shiroDemo {
 
 
         //官方代码给的已经过时
+        /*Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();*/
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
         securityManager.setRealm(new IniRealm("classpath:shiro.ini"));
         SecurityUtils.setSecurityManager(securityManager);
